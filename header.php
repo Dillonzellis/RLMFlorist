6ceb2319138305da729a4d037b31b3efa04dd89d
Dillonzellis <31318165+Dillonzellis@users.noreply.github.com>
2021-02-01 11:08:56 -0500

    understrap theme, header

---
+++ header.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<?php do_action( 'wp_body_open' ); ?>
+	<div class="site" id="page">
+
+		<!-- ******************* The Navbar Area ******************* -->
+		<div id="wrapper-navbar" itemscope itemtype="http://schema.org/WebSite">
+
+			<a class="skip-link sr-only sr-only-focusable"
+				href="#content"><?php esc_html_e( 'Skip to content', 'understrap' ); ?></a>
+
+			<nav class="navbar navbar-expand-lg navbar-light bg-header-bg">
+
+				<h2 id="main-nav-label" class="sr-only">
+					<?php esc_html_e( 'Main Navigation', 'understrap' ); ?>
+				</h2>
+
+				<div class="container-fluid">
+
+					<!-- Your site title as branding in the menu -->
+
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/rlmcrop-150x150.png" alt="logo" srcset="">
+					</a>
+
+					<!-- end custom logo -->
+
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<!-- The WordPress Menu goes here -->
+					<?php wp_nav_menu(
+					array(
+						'theme_location'  => 'primary',
+						'container_class' => 'collapse navbar-collapse',
+						'container_id'    => 'navbarNavDropdown',
+						'menu_class'      => 'navbar-nav ml-auto',
+						'fallback_cb'     => '',
+						'menu_id'         => 'main-menu',
+						'depth'           => 2,
+						'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+					)
+				); ?>
+
+				</div><!-- .container -->
+			</nav><!-- .site-navigation -->
+
+		</div><!-- #wrapper-navbar end -->
