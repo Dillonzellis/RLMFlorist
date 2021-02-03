1699346b01096210a90cd7cc65fabb2adbb798bf
Dillonzellis <31318165+Dillonzellis@users.noreply.github.com>
2021-02-03 00:01:27 -0500

    Fixed Font/nav link size

--- header.php
+++ header.php
@@ -20,7 +20,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
