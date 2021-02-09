5caa55bd3588d565b76b98a9c992f4c9dda1e7c7
Dillonzellis <31318165+Dillonzellis@users.noreply.github.com>
2021-02-09 17:57:59 -0500

    header dropdown

--- header.php
+++ header.php
@@ -43,9 +43,11 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 					<!-- end custom logo -->
 
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
-						<span class="navbar-toggler-icon "></span>
-					</button>
+					<div class="mobile-menu-dropdown">
+						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+							<span class="navbar-toggler-icon "></span>
+						</button>
+					</div>
 
 					<!-- The WordPress Menu goes here -->
 					<?php wp_nav_menu(
