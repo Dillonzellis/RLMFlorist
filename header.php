fc171acb8f6bd25323d22432d4b2075963942f82
Dillonzellis <31318165+Dillonzellis@users.noreply.github.com>
2021-02-11 21:04:54 -0500

    header wrapper cols

--- header.php
+++ header.php
@@ -21,7 +21,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="preconnect" href="https://fonts.gstatic.com">
 	<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+		integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 	<?php wp_head(); ?>
 </head>
 
@@ -29,56 +30,76 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<?php do_action( 'wp_body_open' ); ?>
 	<div class="site" id="page">
 
-		<!-- ******************* The Navbar Area ******************* -->
 		<div id="wrapper-nav">
 
-			<nav id="main-nav" class="navbar navbar-expand-xl bg-header-bg">
+			<div class="top-header">
 
-				<div class="container-fluid">
+				<div class="container-fluid ">
 
-					<!-- Your site title as branding in the menu -->
+					<div class="row custom-flex">
 
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
-						<img src="<?php echo get_template_directory_uri(); ?>/img/rlmcrop-150x150.png" alt="logo" srcset="">
-					</a>
+						<div class="col col-lg-2">
 
-					<!-- end custom logo -->
+							<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
 
-					<div class="container-fluid align-items-center">
+								<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="logo"
+									srcset="">
 
-						<!-- <div class="mobile-menu-dropdown">
-							<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
-								<i class="fas fa-bars fa-2x"></i>
-							</button>
-						</div> -->
+							</a>
 
-						<!-- The WordPress Menu goes here -->
-						<?php wp_nav_menu(
-						array(
-							'theme_location'  => 'primary',
-							'container_class' => 'collapse navbar-collapse',
-							'container_id'    => 'navbarNavDropdown',
-							'menu_class'      => 'navbar-nav ml-auto',
-							'fallback_cb'     => '',
-							'menu_id'         => 'main-menu',
-							'depth'           => 2,
-							'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
-							)
-						); ?>
-
-						<div class="cart-icon-holder">
-							<div class="cart-icon">
-								<a href="#"><i class="fas fa-shopping-cart fa-lg"></i></a>
-							</div>
 						</div>
-						
+
+						<div class="col col-lg-9">
+
+							<nav id="main-nav" class="navbar navbar-expand-xl bg-header-bg">
+
+								<div class="mobile-menu-dropdown">
+									<button class="navbar-toggler" type="button" data-toggle="collapse"
+										data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown"
+										aria-expanded="false"
+										aria-label="<?php esc_attr_e( 'Toggle navigation', 'understrap' ); ?>">
+										<i class="fas fa-bars fa-2x"></i>
+									</button>
+								</div>
+
+								<?php wp_nav_menu(
+									array(
+										'theme_location'  => 'primary',
+										'container_class' => 'collapse navbar-collapse',
+										'container_id'    => 'navbarNavDropdown',
+										'menu_class'      => 'navbar-nav ml-auto',
+										'fallback_cb'     => '',
+										'menu_id'         => 'main-menu',
+										'depth'           => 2,
+										'walker'          => new Understrap_WP_Bootstrap_Navwalker(),
+										)
+								); ?>
+
+							</nav>
+
+						</div>
+
+						<div class="col col-lg-1">
+
+							<div class="cart-icon">
+
+								<a href="#"><i class="fas fa-shopping-cart fa-lg"></i></a>
+
+							</div>
+
+						</div>
 
 					</div>
 
+				</div>
+
+			</div>
 
 
-				</div><!-- .container -->
-			</nav><!-- .site-navigation -->
+
+
+
+
+
 
 		</div><!-- #wrapper-navbar end -->
-		
---
+++ test-header.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+$container = get_theme_mod( 'understrap_container_type' );
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+    <?php do_action( 'wp_body_open' ); ?>
+    <div class="site" id="page">
+
+
+
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"
+                aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Disabled</a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                    <input class="form-control mr-sm-2" type="search" placeholder="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
