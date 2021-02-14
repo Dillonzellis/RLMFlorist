643f259faa1297f9744de2dd9fb0a099e278db48
Dillonzellis <31318165+Dillonzellis@users.noreply.github.com>
2021-02-14 14:02:18 -0500

    shop card and began video card

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -34,12 +34,12 @@ $container = get_theme_mod( 'understrap_container_type' );
         </h3>
       </div>
     </div>
-    <div class="row justify-content-space-around">
-      <div class="col headers">
-        <button type="button" class="btn btn-outline-light headers">770-536-7306</button>
+    <div class="row headers">
+      <div class="col ">
+        <button type="button" class="btn btn-outline-light btn1">770-536-7306</button>
       </div>
-      <div class="col headers">
-        <button type="button" class="btn btn-outline-light headers">Shop Now</button>
+      <div class="col">
+        <button type="button" class="btn btn-outline-light btn2">Shop Now</button>
       </div>
     </div>
     <div class="row">
@@ -50,30 +50,57 @@ $container = get_theme_mod( 'understrap_container_type' );
   </div>
 </div>
 
-<div class="shop-card-holder">
+<div class="shop-card-1-holder">
   <div class="container">
-    <div class="row">
-      <div class="col">
-        <div class="sub-title">the</div>
-        <div class="shop-card-title">
-          valentine's collection
+    <div class="shop-card-titles-holder">
+      <div class="row">
+        <div class="col">
+          <div class="sub-title">the</div>
+          <div class="shop-card-title">
+            valentine's collection
+          </div>
         </div>
       </div>
     </div>
+
+    <div class="card-deck">
+      <div class="card">
+        <img class="card-img-top" src="//www.html.am/images/samples/remarkables_queenstown_new_zealand-300x225.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <button type="button" class="btn btn-light">add to cart</button>
+        </div>
+      </div>
+      <div class="card">
+        <img class="card-img-top" src="//www.html.am/images/samples/remarkables_queenstown_new_zealand-300x225.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <button type="button" class="btn btn-light">add to cart</button>
+        </div>
+      </div>
+      <div class="card">
+        <img class="card-img-top" src="//www.html.am/images/samples/remarkables_queenstown_new_zealand-300x225.jpg" alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <button type="button" class="btn btn-light">add to cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="video-banner-holder">
+  <div class="container">
     <div class="row">
       <div class="col">
-        <div class="card">
-          card 1
+        <div class="video-title">
+          Farm to Table
         </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          card 1
+        <div class="video-sub-title">
+          Fair Trade Flowers
         </div>
-      </div>
-      <div class="col">
-        <div class="card">
-          card 1
+        <div class="video-sub2-title">
+          42 Years of Award Winning Designs
         </div>
       </div>
     </div>
