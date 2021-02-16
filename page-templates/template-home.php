6fb75b4b2f200e4548c4252c46d2603cbc433010
Dillonzellis <31318165+Dillonzellis@users.noreply.github.com>
2021-02-15 21:51:47 -0500

    review box shadows

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -65,21 +65,24 @@ $container = get_theme_mod( 'understrap_container_type' );
 
     <div class="card-deck">
       <div class="card">
-        <img class="card-img-top" src="//www.html.am/images/samples/remarkables_queenstown_new_zealand-300x225.jpg" alt="Card image cap">
+        <img class="card-img-top" src="//www.html.am/images/samples/remarkables_queenstown_new_zealand-300x225.jpg"
+          alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <button type="button" class="btn btn-light">add to cart</button>
         </div>
       </div>
       <div class="card">
-        <img class="card-img-top" src="//www.html.am/images/samples/remarkables_queenstown_new_zealand-300x225.jpg" alt="Card image cap">
+        <img class="card-img-top" src="//www.html.am/images/samples/remarkables_queenstown_new_zealand-300x225.jpg"
+          alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <button type="button" class="btn btn-light">add to cart</button>
         </div>
       </div>
       <div class="card">
-        <img class="card-img-top" src="//www.html.am/images/samples/remarkables_queenstown_new_zealand-300x225.jpg" alt="Card image cap">
+        <img class="card-img-top" src="//www.html.am/images/samples/remarkables_queenstown_new_zealand-300x225.jpg"
+          alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <button type="button" class="btn btn-light">add to cart</button>
@@ -122,21 +125,24 @@ $container = get_theme_mod( 'understrap_container_type' );
 
     <div class="card-deck">
       <div class="card">
-        <img class="card-img-top" src="//www.html.am/images/samples/remarkables_queenstown_new_zealand-300x225.jpg" alt="Card image cap">
+        <img class="card-img-top" src="//www.html.am/images/samples/remarkables_queenstown_new_zealand-300x225.jpg"
+          alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <button type="button" class="btn btn-light">add to cart</button>
         </div>
       </div>
       <div class="card">
-        <img class="card-img-top" src="//www.html.am/images/samples/remarkables_queenstown_new_zealand-300x225.jpg" alt="Card image cap">
+        <img class="card-img-top" src="//www.html.am/images/samples/remarkables_queenstown_new_zealand-300x225.jpg"
+          alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <button type="button" class="btn btn-light">add to cart</button>
         </div>
       </div>
       <div class="card">
-        <img class="card-img-top" src="//www.html.am/images/samples/remarkables_queenstown_new_zealand-300x225.jpg" alt="Card image cap">
+        <img class="card-img-top" src="//www.html.am/images/samples/remarkables_queenstown_new_zealand-300x225.jpg"
+          alt="Card image cap">
         <div class="card-body">
           <h5 class="card-title">Card title</h5>
           <button type="button" class="btn btn-light">add to cart</button>
@@ -148,21 +154,21 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 <div class="cta-banner-holder">
   <div class="container">
-      <div class="row">
-        <div class="col">
-          <div class="cta-title">
-            Special Occasion?
-          </div>
-          <div class="cta-sub-title">
-            Check out our Event Services
-          </div>
+    <div class="row">
+      <div class="col">
+        <div class="cta-title">
+          Special Occasion?
+        </div>
+        <div class="cta-sub-title">
+          Check out our Event Services
         </div>
       </div>
-      <div class="row">
-        <div class="col text-center">
-          <button type="button" class="btn btn-lg btn-outline-light">RLM Venue & Event Services</button>
-        </div>
+    </div>
+    <div class="row">
+      <div class="col text-center">
+        <button type="button" class="btn btn-lg btn-outline-light">RLM Venue & Event Services</button>
       </div>
+    </div>
   </div>
 </div>
 
@@ -181,14 +187,15 @@ $container = get_theme_mod( 'understrap_container_type' );
       </div>
     </div>
     <div class="card-deck">
-
       <div class="card">
-        <div class="card-body">
-          <div class="card-review">
-            RLM did the flowers, decor for reception and the cake for my daughters wedding. They exceeding our expectation - the flowers were gorgeous, the venue decor was stunning and the cake not only was beautiful it tasted GREAT. They were very organized, responsive and the day off made things super easy! Thanks again.
-          </div>
-          <div class="review-customer-holder">
-            <div class="review-customer-img">
+          <div class="card-body">
+            <div class="card-review">
+              RLM did the flowers, decor for reception and the cake for my daughters wedding. They exceeding our expectation
+              - the flowers were gorgeous, the venue decor was stunning and the cake not only was beautiful it tasted GREAT.
+              They were very organized, responsive and the day off made things super easy! Thanks again.
+            </div>
+            <div class="review-customer-holder">
+              <div class="review-customer-img">
                 [img]
               </div>
               <div class="review-customer">
@@ -197,48 +204,49 @@ $container = get_theme_mod( 'understrap_container_type' );
               <div class="review-customer-sub-line">
                 Mother of the Bride
               </div>
+            </div>
           </div>
         </div>
-      </div>
-
       <div class="card">
         <div class="card-body">
           <div class="card-review">
-            RLM did the flowers, decor for reception and the cake for my daughters wedding. They exceeding our expectation - the flowers were gorgeous, the venue decor was stunning and the cake not only was beautiful it tasted GREAT. They were very organized, responsive and the day off made things super easy! Thanks again.
+            RLM did the flowers, decor for reception and the cake for my daughters wedding. They exceeding our expectation
+            - the flowers were gorgeous, the venue decor was stunning and the cake not only was beautiful it tasted GREAT.
+            They were very organized, responsive and the day off made things super easy! Thanks again.
           </div>
           <div class="review-customer-holder">
             <div class="review-customer-img">
-                [img]
-              </div>
-              <div class="review-customer">
-                Colheen R
-              </div>
-              <div class="review-customer-sub-line">
-                Mother of the Bride
-              </div>
+              [img]
+            </div>
+            <div class="review-customer">
+              Colheen R
+            </div>
+            <div class="review-customer-sub-line">
+              Mother of the Bride
+            </div>
           </div>
         </div>
       </div>
-      
       <div class="card">
         <div class="card-body">
           <div class="card-review">
-            RLM did the flowers, decor for reception and the cake for my daughters wedding. They exceeding our expectation - the flowers were gorgeous, the venue decor was stunning and the cake not only was beautiful it tasted GREAT. They were very organized, responsive and the day off made things super easy! Thanks again.
+            RLM did the flowers, decor for reception and the cake for my daughters wedding. They exceeding our expectation
+            - the flowers were gorgeous, the venue decor was stunning and the cake not only was beautiful it tasted GREAT.
+            They were very organized, responsive and the day off made things super easy! Thanks again.
           </div>
           <div class="review-customer-holder">
             <div class="review-customer-img">
-                [img]
-              </div>
-              <div class="review-customer">
-                Colheen R
-              </div>
-              <div class="review-customer-sub-line">
-                Mother of the Bride
-              </div>
+              [img]
+            </div>
+            <div class="review-customer">
+              Colheen R
+            </div>
+            <div class="review-customer-sub-line">
+              Mother of the Bride
+            </div>
           </div>
         </div>
       </div>
-
     </div>
   </div>
 </div>
@@ -263,10 +271,14 @@ $container = get_theme_mod( 'understrap_container_type' );
           Covid-19
         </div>
         <div class="covid-body">
-          <p>We are grateful for all of you and are open for phone or internet orders. They will be fulfilled with a no contact delivery left at your front door or a no contact pick up at our front door.</p>
+          <p>We are grateful for all of you and are open for phone or internet orders. They will be fulfilled with a no
+            contact delivery left at your front door or a no contact pick up at our front door.</p>
         </div>
         <div class="covid-body">
-          We realize that this is a very stressful time, and we are here for your floral, event, BabyCakes &amp; venue needs. We are monitoring the situation closely and will update you with any changes as they arise. We’re closely monitoring the Center for Disease Control’s guidelines and recommendations on additional steps to take to help stop the spread of the virus.
+          We realize that this is a very stressful time, and we are here for your floral, event, BabyCakes &amp; venue
+          needs. We are monitoring the situation closely and will update you with any changes as they arise. We’re
+          closely monitoring the Center for Disease Control’s guidelines and recommendations on additional steps to take
+          to help stop the spread of the virus.
         </div>
         <hr>
       </div>
