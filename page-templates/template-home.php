2a4107ac3b7d5d78008b36980e24e2049383ab78
Dillon Ellis <dillonzellis@gmail.com>
2021-02-20 12:36:42 -0500

    worked wth carousel progress 1

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -358,56 +358,113 @@ $container = get_theme_mod( 'understrap_container_type' );
       <p>carousel</p>
     </div>
   </div>
+  <div class="container-fluid">
+    <div class="test-holder">
+      <div id="carouseldz" class="carousel slide" data-ride="carousel" data-pause="true" data-interval="3000">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img class="" src="..." alt="">
+            <div class="carousel-caption">
+              <div class="worked-with-logo">
+                logo 1
+              </div>
+              <div class="worked-with-logo">
+                logo 2
+              </div>
+              <div class="worked-with-logo">
+                logo 3
+              </div>
+              <div class="worked-with-logo">
+                logo 4
+              </div>
+              <div class="worked-with-logo">
+                logo 5
+              </div>
+            </div>
+          </div>
+          <div class="carousel-item">
+            <img class="" src="..." alt="">
+            <div class="carousel-caption">
 
-  <div class="test-holder">
-    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner">
-        <div class="carousel-item active">
-          <img class="d-block w-100" src="..." alt="First slide">
-          <div class="carousel-caption">
-            <div class="worked-with-logo">
-              logo 1
+              <div class="worked-with-logo">
+                logo 1
+              </div>
+              <div class="worked-with-logo">
+                logo 2
+              </div>
+              <div class="worked-with-logo">
+                logo 3
+              </div>
+              <div class="worked-with-logo">
+                logo 4
+              </div>
+              <div class="worked-with-logo">
+                logo 5
+              </div>
+
+
             </div>
-            <div class="worked-with-logo">
-              logo 2
-            </div>
-            <div class="worked-with-logo">
-              logo 3
-            </div>
-            <div class="worked-with-logo">
-              logo 4
-            </div>
-            <div class="worked-with-logo">
-              logo 5
+          </div>
+          <div class="carousel-item">
+            <img class="" src="..." alt="">
+            <div class="carousel-caption">
+
+              <div class="worked-with-logo">
+                logo 1
+              </div>
+              <div class="worked-with-logo">
+                logo 2
+              </div>
+              <div class="worked-with-logo">
+                logo 3
+              </div>
+              <div class="worked-with-logo">
+                logo 4
+              </div>
+              <div class="worked-with-logo">
+                logo 5
+              </div>
+
+
             </div>
           </div>
         </div>
-        <div class="carousel-item">
-          <img class="d-block w-100" src="..." alt="Second slide">
-        </div>
-        <div class="carousel-item">
-          <img class="d-block w-100" src="..." alt="Third slide">
-        </div>
+        <a class="carousel-control-prev" data-target="#carouseldz" role="button" data-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true">prev</span>
+        </a>
+        <a class="carousel-control-next" data-target="#carouseldz" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true">next</span>
+        </a>
       </div>
-      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-      </a>
-      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-      </a>
     </div>
   </div>
-
-
-
 </div>
+
+
+<!-- <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="..." alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="..." alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div> -->
+
+
+
 
 <div class="covid-holder">
   <div class="container">
