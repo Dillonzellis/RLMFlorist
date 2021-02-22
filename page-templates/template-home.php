c352ece9b3f39290f2e67040dc4778aeba548190
Dillonzellis <31318165+Dillonzellis@users.noreply.github.com>
2021-02-22 11:19:43 -0500

    CTA fade ins

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -247,7 +247,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 <div class="cta-banner-holder">
   <div class="container">
     <div class="row">
-      <div class="col">
+      <div class="col fade-in">
         <div class="cta-title">
           Special Occasion?
         </div>
@@ -257,7 +257,7 @@ $container = get_theme_mod( 'understrap_container_type' );
       </div>
     </div>
     <div class="row">
-      <div class="col text-center">
+      <div class="col text-center fade-in">
         <button type="button" class="btn btn-lg btn-outline-light">RLM Venue & Event Services</button>
       </div>
     </div>
@@ -470,31 +470,6 @@ $container = get_theme_mod( 'understrap_container_type' );
     </a>
   </div>
 </div>
-
-
-<!-- <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="..." alt="First slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Second slide">
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="..." alt="Third slide">
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div> -->
-
-
 
 
 <div class="covid-holder">
