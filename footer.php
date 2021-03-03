e7000c0d233101cfef38d2d53a5fb55b91832f4d
Dillonzellis <31318165+Dillonzellis@users.noreply.github.com>
2021-03-03 17:17:15 -0500

    finished responsive footer

--- footer.php
+++ footer.php
@@ -21,22 +21,22 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 
 
-<div class="wrapper pt-1" id="wrapper-footer">
+<div class="wrapper pt-1 pb-2" id="wrapper-footer">
 
     <hr>
 
     <div class="contact-holder container">
-        <div class="row justify-content-center">
+        <div class="row justify-content-center align-items-center">
             <div class="col-lg-2">
                 <a href="<?php echo esc_url( home_url( '/' ) ); ?>">
                     <img loading=lazy src="<?php echo get_template_directory_uri(); ?>/img/rlmfooterwhite.png" alt="logo">
                 </a>
             </div>
             <div class="col-lg-2">
-                <div class="contact">(770) 536-7306</div>
+                <a href="tel:770-536-7306" class="contact">(770) 536-7306</a>
             </div>
             <div class="col-lg-2">
-                <div class="contact">RESIA@RLMAFFAIRS.COM</div>
+                <a class="contact" href="mailto:resia@rlmaffairs.com">EMAIL US</a> 
             </div>
         </div>
     </div>
@@ -79,37 +79,30 @@ $container = get_theme_mod( 'understrap_container_type' );
     <div class="socials-holder container">
       <div class="row justify-content-center">
         <div class="col-2">
-            <div class="social"><a href=""><i class="fab fa-facebook-f fa-lg"></i></a></div>
+            <div class="social"><a href="#"><i class="fab fa-facebook-f fa-lg"></i></a></div>
         </div>
         <div class="col-2">
-            <div class="social"><a href=""><i class="fab fa-instagram fa-lg"></i></a></div>
+            <div class="social"><a href="#"><i class="fab fa-instagram fa-lg"></i></a></div>
         </div>
         <div class="col-2">
-            <div class="social"><a href=""><i class="far fa-envelope fa-lg"></i></a></div>
+            <div class="social"><a href="mailto:resia@rlmaffairs.com"><i class="far fa-envelope fa-lg"></i></a></div>
         </div>
       </div>
     </div>
 
     <div class="legal-holder container">
-        <div class="row justify-content-center mt-6">
-            <div class="col-2">
+        <div class="row justify-content-center align-items-center">
+            <div class="col-sm-4 col-md-2">
                 <div class="legal-item"><a href="#">Terms of Service</a></div>
             </div>
-            <div class="col-3">
+            <div class="col-sm-4 col-md-2">
                 <div class="legal-item">Copyright &#169; <?php echo date('Y'); ?> RLM Florist</div>
             </div>
-            <div class="col-2">
+            <div class="col-sm-4 col-md-2">
                 <div class="legal-item"><a href="#">Privacy Policy</a></div>
             </div>
         </div>
     </div>
-
-      <!-- <div class="legal-holder">
-        <div class="legal-item"><a href="">Terms of Service</a></div>
-        <div class="legal-item">Copyright &#169; <?php echo date('Y'); ?> RLM Florist</div>
-        <div class="legal-item"><a href="">Privacy Policy</a></div>
-    </div> -->
-
 </div><!-- wrapper end -->
 
 </div><!-- #page we need this extra closing tag here -->
