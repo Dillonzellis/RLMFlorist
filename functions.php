c6c585369967a7b0472538da4345ca2eaa0e3ebf
Dillonzellis <31318165+Dillonzellis@users.noreply.github.com>
2021-03-08 20:41:01 -0500

    finished single product styling

--- functions.php
+++ functions.php
@@ -45,8 +45,9 @@ function woocommerce_header_add_to_cart_fragment( $fragments ) {
 	ob_start();
 
 	?>
-	<a class="cart-icon-number" href="<?php echo esc_url(wc_get_cart_url()); ?>"><?php echo WC()->cart->get_cart_contents_count();?></a>
-	<?php
+<a class="cart-icon-number"
+	href="<?php echo esc_url(wc_get_cart_url()); ?>"><?php echo WC()->cart->get_cart_contents_count();?></a>
+<?php
 	$fragments['a.cart-icon-number'] = ob_get_clean();
 	return $fragments;
 }
@@ -60,3 +61,37 @@ function my_woocommerce_widget_shopping_cart_button_view_cart() {
 }
 
 add_action( 'woocommerce_widget_shopping_cart_buttons', 'my_woocommerce_widget_shopping_cart_button_view_cart', 10 );
+
+/**
+ * Change number of related products output
+ */ 
+function woo_related_products_limit() {
+	global $product;
+	  
+	  $args['posts_per_page'] = 6;
+	  return $args;
+  }
+  add_filter( 'woocommerce_output_related_products_args', 'jk_related_products_args', 20 );
+	function jk_related_products_args( $args ) {
+	  $args['posts_per_page'] = 4; // 4 related products
+	  $args['columns'] = 4; // arranged in 2 columns
+	  return $args;
+  	}
+
+
+  // Exclude categories from single-product page related products
+add_filter( 'woocommerce_related_products', 'exclude_product_category_from_related_products', 10, 3 );
+	function exclude_product_category_from_related_products( $related_posts, $product_id, $args  ){
+		// HERE define your product category slug
+		$term_slug = 'happy-holiday';
+
+		// Get the product Ids in the defined product category
+		$exclude_ids = wc_get_products( array(
+			'status'    => 'publish',
+			'limit'     => -1,
+			'category'  => array($term_slug),
+			'return'    => 'ids',
+		) );
+
+		return array_diff( $related_posts, $exclude_ids );
+	}
