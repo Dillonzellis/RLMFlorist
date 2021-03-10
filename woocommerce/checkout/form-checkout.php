2b7c439500992e2f9cc823ff7b89eabbb512aec0
Dillonzellis <31318165+Dillonzellis@users.noreply.github.com>
2021-03-09 22:13:41 -0500

    finished checkout stlying

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -23,12 +23,47 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<div class="checkout-header-wrapper">
 		<img loading=lazy src="<?php echo get_template_directory_uri(); ?>/img/about-us-logo.png" alt="">
 
+		<h4 class="title">Our shop serves the following areas in Georgia: Flowery Branch, Gainesville, Buford, Hoschton, Braselton,
+			Winder and Oakwood.</h4>
+
+		<h5 class="subtitle">***In Store Pickups and Deliveries are only available during store hours.***</h5>
+		<div class="hours-title">
+			Store Hours
+		</div>
+		<div class="hours">
+			Monday: Call in only, showroom closed.
+		</div>
+		<div class="hours">
+			Tuesday: 10:00am - 4:00pm
+		</div>
+		<div class="hours">
+			Wednesday: 10:00am - 4:00pm
+		</div>
+		<div class="hours">
+			Thursday: 10:00am - 4:00pm
+		</div>
+		<div class="hours">
+			Friday: 10:00am - 4:00pm
+		</div>
+		<div class="hours">
+			Saturday: 10:00am - 12:00pm
+		</div>
+
+		<h5 class="post-hours">
+			RLM Florist offers 24 hour phone and internet service for your orders and questions after the showroom has 	closed.
+		</h5>
+
+		<div class="store-phone">
+			770-536-7306
+		</div>
+
+		<h1>Checkout</h1>
+	
+		
 
 	</div>
-	
-	
-	<h1>testingngis</h1>
-<?php
+
+	<?php
 	do_action( 'woocommerce_before_checkout_form', $checkout );
 
 	// If checkout registration is disabled and not logged in, the user cannot checkout.
@@ -39,23 +74,24 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 	?>
 
-	<form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
+	<form name="checkout" method="post" class="checkout woocommerce-checkout"
+		action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
 
 		<?php if ( $checkout->get_checkout_fields() ) : ?>
 
-			<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
+		<?php do_action( 'woocommerce_checkout_before_customer_details' ); ?>
 
-			<div class="row" id="customer_details">
-				<div class="col-12 col-sm-7">
-					<?php do_action( 'woocommerce_checkout_billing' ); ?>
-				</div>
-
-				<div class="col-12 col-sm-5">
-					<?php do_action( 'woocommerce_checkout_shipping' ); ?>
-				</div>
+		<div class="row" id="customer_details">
+			<div class="col-12 col-sm-7">
+				<?php do_action( 'woocommerce_checkout_billing' ); ?>
 			</div>
 
-			<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
+			<div class="col-12 col-sm-5">
+				<?php do_action( 'woocommerce_checkout_shipping' ); ?>
+			</div>
+		</div>
+
+		<?php do_action( 'woocommerce_checkout_after_customer_details' ); ?>
 
 		<?php endif; ?>
 
