b43bd30713922b03e8bb1aefeea793ab0897329d
Dillon Ellis <dillonzellis@gmail.com>
2021-05-11 11:01:07 -0400

    minimun amount added for checkout

--- functions.php
+++ functions.php
@@ -95,3 +95,37 @@ add_filter( 'woocommerce_related_products', 'exclude_product_category_from_relat
 
 		return array_diff( $related_posts, $exclude_ids );
 	}
+
+	/**
+ * Set a minimum order amount for checkout
+ */
+add_action( 'woocommerce_checkout_process', 'wc_minimum_order_amount' );
+add_action( 'woocommerce_before_cart' , 'wc_minimum_order_amount' );
+ 
+function wc_minimum_order_amount() {
+    // Set this variable to specify a minimum order value
+    $minimum = 25;
+
+    if ( WC()->cart->total < $minimum ) {
+
+        if( is_cart() ) {
+
+            wc_print_notice( 
+                sprintf( 'Your current order total is %s — you must have an order with a minimum of %s to place your order ' , 
+                    wc_price( WC()->cart->total ), 
+                    wc_price( $minimum )
+                ), 'error' 
+            );
+
+        } else {
+
+            wc_add_notice( 
+                sprintf( 'Your current order total is %s — you must have an order with a minimum of %s to place your order' , 
+                    wc_price( WC()->cart->total ), 
+                    wc_price( $minimum )
+                ), 'error' 
+            );
+
+        }
+    }
+}
