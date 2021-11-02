0643f2adf9c423f6d4f19ee3fe5a2f5d38e0a082
Dillon Ellis <dillonzellis@gmail.com>
2021-11-01 20:56:34 -0400

    Update minimun amount for delivery and not local pickup

--- functions.php
+++ functions.php
@@ -130,40 +130,6 @@ function exclude_product_category_from_related_products(
     return array_diff($related_posts, $exclude_ids);
 }
 
-/**
- * Set a minimum order amount for checkout
- */
-add_action("woocommerce_checkout_process", "wc_minimum_order_amount");
-add_action("woocommerce_before_cart", "wc_minimum_order_amount");
-
-function wc_minimum_order_amount()
-{
-    // Set this variable to specify a minimum order value
-    $minimum = 34.99;
-
-    if (WC()->cart->total < $minimum) {
-        if (is_cart()) {
-            wc_print_notice(
-                sprintf(
-                    "Your current order total is %s — you must have an order with a minimum of %s to place your order ",
-                    wc_price(WC()->cart->total),
-                    wc_price($minimum)
-                ),
-                "error"
-            );
-        } else {
-            wc_add_notice(
-                sprintf(
-                    "Your current order total is %s — you must have an order with a minimum of %s to place your order",
-                    wc_price(WC()->cart->total),
-                    wc_price($minimum)
-                ),
-                "error"
-            );
-        }
-    }
-}
-
 // remove optional text on checkout fields
 add_filter(
     "woocommerce_form_field",
@@ -232,3 +198,41 @@ function cart_update_script() {
     <?php
     endif;
 }
+
+
+/**
+ * Set a minimum order amount for checkout
+ */
+add_action("woocommerce_checkout_process", "wc_minimum_order_amount");
+// add_action("woocommerce_before_cart", "wc_minimum_order_amount");
+
+function wc_minimum_order_amount()
+{
+    
+    // Set this variable to specify a minimum order value
+    $minimum = 34.99;
+
+    $chosen_shipping = WC()->session->get( 'chosen_shipping_methods' )[0];
+
+    if (WC()->cart->total < $minimum && $chosen_shipping != 'local_pickup:7') {
+        if (is_cart()) {
+            wc_print_notice(
+                sprintf(
+                    "Your current order total is %s — you must have an order with a minimum of %s to place your order for delivery",
+                    wc_price(WC()->cart->total),
+                    wc_price($minimum)
+                ),
+                "error"
+            );
+        } else {
+            wc_add_notice(
+                sprintf(
+                    "Your current order total is %s — you must have an order with a minimum of %s to place your order for delivery",
+                    wc_price(WC()->cart->total),
+                    wc_price($minimum)
+                ),
+                "error"
+            );
+        }
+    }    
+}    
