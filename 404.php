3911ae2e24067dc6ebe8127b0ca151c907bc576c
Dillon Ellis <dillonzellis@gmail.com>
2022-01-10 23:42:37 -0500

    404 page styling

--- 404.php
+++ 404.php
@@ -12,7 +12,7 @@ get_header();
 
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
-
+<div class="container">
 <div class="wrapper" id="error-404-wrapper">
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
@@ -30,16 +30,16 @@ $container = get_theme_mod( 'understrap_container_type' );
 							<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'understrap' ); ?></h1>
 
 						</header><!-- .page-header -->
+						
+							<div class="page-content">
 
-						<div class="page-content">
+							<p><?php esc_html_e( 'It looks like nothing was found at this location.', 'understrap' ); ?></p>
 
-							<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'understrap' ); ?></p>
+							<!-- <?php get_search_form(); ?> -->
 
-							<?php get_search_form(); ?>
+							<!-- <?php the_widget( 'WP_Widget_Recent_Posts' ); ?> -->
 
-							<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
-
-							<?php if ( understrap_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
+							<!-- <?php if ( understrap_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
 
 								<div class="widget widget_categories">
 
@@ -61,18 +61,20 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 								</div><!-- .widget -->
 
-							<?php endif; ?>
+							<!-- <?php endif; ?>  -->
 
 							<?php
 
 							/* translators: %1$s: smiley */
-							$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'understrap' ), convert_smilies( ':)' ) ) . '</p>';
-							the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
+							// $archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'understrap' ), convert_smilies( ':)' ) ) . '</p>';
+							// the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
 
-							the_widget( 'WP_Widget_Tag_Cloud' );
-							?>
+							// the_widget( 'WP_Widget_Tag_Cloud' );
+							// ?>
 
-						</div><!-- .page-content -->
+							</div><!-- .page-content -->
+						</div>
+	
 
 					</section><!-- .error-404 -->
 
@@ -85,5 +87,8 @@ $container = get_theme_mod( 'understrap_container_type' );
 	</div><!-- #content -->
 
 </div><!-- #error-404-wrapper -->
+</div>
+
+
 
 <?php get_footer(); ?>
