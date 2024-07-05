6657c63f3413d1e303c96399f5c6c3a4ac740423
dillonzellis <dillonzellis@gmail.com>
2024-07-05 10:12:12 -0400

    add catering page and remove gumbo and souffle

---
+++ page-templates/catering.php
@@ -0,0 +1,287 @@
+<?php
+/**
+ * Template Name: Catering Template
+ *
+ *
+ * @package UnderStrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+get_header();
+?>
+
+<div class="container">
+    <div class="row">
+        <p>
+            RLM Affairs offers all the services you may need for your event. Our staff works directly with our clients to tailor each event to your specific needs. Whether you need just catering, or a complete event design, we will work with you as much as you need, or we will provide you with A la carte services based specifically on your needs and budget. Our award winning catering, wedding planning, cakes, and floral design are available every day, breakfast, lunch, dinner, or in between. 
+        </p>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <img class="menu-header-img " src="<?php echo get_template_directory_uri(); ?>/img/veri.jpg" alt="" srcset=""  width="auto" height="230">
+    </div>
+    <div class="row">
+        <h1>Catering Menu</h1>
+    </div>
+    <div class="row mobile-headers">
+        <h2 class="main-header">Hors D'oeuvres Stations</h2>
+    </div>
+    <div class="row mb-5 align-items-center">
+        <div class="col-md">
+            <img class="img-blur" src="<?php echo get_template_directory_uri(); ?>/img/charc.jpg" alt="" srcset=""  width="auto" height="230">
+        </div>
+        <div class="col-md">
+            <div class="menu-items">
+                <ul>
+                    <li>
+                        Assorted Crostini: Toasted Breads, with a delicious variety of toppings
+                    </li>
+                    <li>
+                        Fresh Fruit platters with a bountiful seasonal variety
+                    </li>
+                    <li>
+                        Assorted Garden Fresh Seasonal Sliced Veggies: served with ranch or hummus
+                    </li>
+                    <li>
+                        Artistic Cheese Platters: may include cheddar, pepper jack, brie optiotns with dried apricots, cranberries, chopped herbs, drizzle clover honey, and other assorted cheeses included with assorted crackers
+                    </li>
+                    <li>
+                        Buffalo Chicken Dip Served with crusted breads
+                    </li>
+                    <li>
+                        Hot Artichokes and Fresh Spinach dip served with organic and blue chips
+                    </li>
+                    <li>
+                        Stuffed Mushroom Caps with meats or vegetarian options
+                    </li>
+                    <li>
+                        Manhattan Meatballs: house made meatballs with a sweet spicy glaze
+                    </li>
+                    <li>
+                        Jumbo shrimp with house made cocktail sauce
+                    </li>
+                    <li>
+                        Pigs in a Blanket
+                    </li>
+                    <li>
+                        Shot Glass Salads ~ Grilled Corn, Black Eyed Pea, Zucchini(or any Squash)
+                    </li>
+                    <li>
+                        Shot glass of Cool or Chilled Soups
+                    </li>
+                    <li>
+                        Stuffed Cucumber Bites
+                    </li>
+                    <li>
+                        Mini Quiches - variety of fillings
+                    </li>
+                    <li>
+                        The Exquisite Charcuterie Experience(counts as 2 appetizers)
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="row mobile-headers">
+        <h2 class="sub-header">Salad Selections</h2>
+    </div>
+    <div class="row mb-5 align-items-center">
+        <div class="col-md">
+            <div class="menu-items">
+                <ul>
+                    <li>
+                        Classic Caesar
+                    </li>
+                    <li>
+                        Garden lettuce mix tomato cucumber carrots croutons & choice of dressing
+                    </li>
+                    <li>
+                        Baby organic spring mix Salad with pecans, cucumber, dried cranberries and house balsamic vinaigrette
+                    </li>
+                    <li>
+                        Romaine, spinach, and iceberg topped with bacon, cucumber, tomato, bleu cheese crumbles, and a balsamic drizzle                    
+                    </li>
+                    <li>
+                        Garden romaine and iceberg topped with fresh vegetables and served with your choice of dressing
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md">
+            <img class="salad img-blur" src="<?php echo get_template_directory_uri(); ?>/img/salads.jpg" alt="" srcset=""  width="380" height="270">
+        </div>
+    </div>
+    <div class="row mobile-headers">
+        <h2 class="sub-header">Buffet Main Course Options</h2>
+    </div>
+    <div class="row mb-5 align-items-center">
+        <div class="col-md">
+            <div class="menu-items">
+                <ul>
+                    <li>
+                        Sesame Sirloin with caramelized sweet sesame drizzle
+                    </li>
+                    <li>
+                        Smoked Pulled Pork with shredded crisp onions
+                    </li>
+                    <li>
+                        Traditional Meat Lasagna with Italian sausage, beef, and four cheeses
+                    </li>
+                    <li>
+                        Roast Herb Crusted Pork Loin with mango chutney
+                    </li>
+                    <li>
+                        Asiago Beef Strips Herb crusted thin sliced strips topped asiago cheese
+                    </li>
+                    <li>
+                        Mediterranean Beef Tenderloin with onions, tomatoes, peppers, garlic, capers, and herbs
+                    </li>
+                    <li>
+                        Chicken & Basmati Rice Casserole
+                    </li>
+                    <li>
+                        Chicken florentine with spinach, mushrooms, and asiago cream sauce
+                    </li>
+                    <li>
+                        Chicken Caprese with mozzarella tomatoes basil & pesto
+                    </li>
+                    <li>
+                        BBQ Boneless Chicken Breast
+                    </li>
+                    <li>
+                        Roasted vegetable Lasagna with four Italian cheeses and fresh herbs in a zesty marinara
+                    </li>
+                    <li>
+                        Mediterranean Penne Pasta with onions, peppers, artichokes, capers, tomatoes, and fresh herbs
+                    </li>
+                    <li>
+                        Fettuccine Alfredo with four cheeses and basil
+                    </li>
+                    <li>
+                        Tricolored Tortellini with your choice of marinara, alfredo, or pesto
+                    </li>
+                    <li>
+                        North American Salmon Filet with fresh herbs and a buttery champagne sauce(seasonal pricing)
+                    </li>
+                    <li>
+                        Classic Southern Jumbo Shrimp & Grits
+                    </li>
+                    <li>
+                        Tortellini with pesto, shrimp or chicken with spinach and asiago cheese in a champagne cream sauce
+                    </li>
+                    <li>
+                        Curry spiced Tilapia with citrus basil butter
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md">
+            <img class ="buffet img-blur" src="<?php echo get_template_directory_uri(); ?>/img/Chicken.jpg" alt="" srcset=""  width="auto" height="230">
+        </div>
+    </div>
+    <div class="row mobile-headers">
+        <h2 class="sub2-header">Side Dishes</h2>
+    </div>
+    <div class="row mb-5 align-items-center">
+        <div class="col-md">
+            <img class="asparagus img-blur" src="<?php echo get_template_directory_uri(); ?>/img/asparagus.jpg" alt="" srcset=""  width="auto" height="420">
+        </div>
+        <div class="col-md">
+            <div class="menu-items">
+                <ul>
+                    <li>
+                        Garlic smashed potatoes
+                    </li>
+                    <li>
+                        Potatoes au Gratin
+                    </li>
+                    <li>
+                        Steam Asparagus(seasonal price)
+                    </li>
+                    <li>
+                        Steamed assorted Vegetables
+                    </li>
+                    <li>
+                        Squash Casserole
+                    </li>
+                    <li>
+                        Smashed sweet potatoes
+                    </li>
+                    <li>
+                        Green Bean Casserole
+                    </li>
+                    <li>
+                        Southern Green Beans
+                    </li>
+                    <li>
+                        Glazed Baby Carrots
+                    </li>
+                    <li>
+                        Roasted Vegetables
+                    </li>
+                    <li>
+                        Cream Corn
+                    </li>
+                    <li>
+                        Chef's blend wild rice
+                    </li>
+                    <li>
+                        Jasmine rice
+                    </li>
+                    <li>
+                        Basmati rice
+                    </li>
+                    <li>
+                        Tomato cucumber salad
+                    </li>
+                    <li>
+                        Creamy Risotto
+                    </li>
+                    <li>
+                        Gourmet Mac and Cheese
+                    </li>
+                    <li>
+                        Roasted Sweet Corn and Tomato Salad
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="row mb-5">
+        <div class="col-md">
+            <img class="img-blur"src="<?php echo get_template_directory_uri(); ?>/img/chicken-taco.jpg" alt="" srcset=""  width="auto" height="230">
+        </div>
+        <div class="col-md stations">
+            <h2>Trend Stations:</h2>
+            <div class="menu-items">
+                <ul>
+                    <li>
+                        Classic Taco Bar
+                    </li>
+                    <li>
+                        Street Taco Bar
+                    </li>
+                    <li>
+                        Grits Bar
+                    </li>
+                    <li>
+                        Mac & Cheese Bar
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+<div class="disclaimer-wrapper">
+    <div class="disclaimer">*All red meat and seafood are upgrades with any package*</div>
+    <div class="disclaimer">*Menu availability and pricing subject to change without notice*</div> 
+</div>
+
+
+</div>
+
+
+<?php get_footer();
